60c3458572bff8fa1c223b23199f2c8fa1f8ad4c
kimternet <kimternet23@gmail.com>
2025-04-05 14:53:54 +0900

    네비바 버튼 수정

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -62,58 +62,54 @@ const NavButton = ({
             className="absolute cursor-pointer z-50"
             style={{ transform: `translate(${x}, ${y})` }}
           >
-            <NavLink
-              variants={item}
-              href={link}
-              target={newTab ? "_blank" : "_self"}
-              className="text-foreground  rounded-full flex items-center justify-center
-        custom-bg
-        "
-              aria-label={label}
-              name={label}
-              prefetch={false}
-              scroll={false}
-            >
-              <span className="relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent">
-                {getIcon(icon)}
-
-                <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
-
-                <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
-                  {label}
+            <div className="flex flex-col items-center">
+              <NavLink
+                variants={item}
+                href={link}
+                target={newTab ? "_blank" : "_self"}
+                className="text-foreground rounded-full flex items-center justify-center custom-bg"
+                aria-label={label}
+                name={label}
+                prefetch={false}
+                scroll={false}
+              >
+                <span className="relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent">
+                  {getIcon(icon)}
                 </span>
+              </NavLink>
+              
+              <span className="text-xs mt-2 px-2 py-1 bg-background/30 backdrop-blur-sm text-foreground rounded-md shadow-lg animate-spin-slow-reverse group-hover:pause whitespace-nowrap">
+                {label}
               </span>
-            </NavLink>
+            </div>
           </div>
         ) : (
           <div className="w-fit cursor-pointer z-50">
-            <NavLink
-              variants={item}
-              href={link}
-              target={newTab ? "_blank" : "_self"}
-              className="text-foreground  rounded-full flex items-center justify-center
-        custom-bg
-        "
-              aria-label={label}
-              name={label}
-              prefetch={false}
-              scroll={false}
-            >
-              <span className="relative  w-10 h-10  xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent">
-                {getIcon(icon)}
-
-                <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
-
-                <span
-                  className={clsx(
-                    "absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap",
-                    labelDirection === "left" ? "right-full left-auto" : ""
-                  )}
-                >
-                  {label}
+            <div className="flex items-center space-x-2">
+              <NavLink
+                variants={item}
+                href={link}
+                target={newTab ? "_blank" : "_self"}
+                className="text-foreground rounded-full flex items-center justify-center custom-bg"
+                aria-label={label}
+                name={label}
+                prefetch={false}
+                scroll={false}
+              >
+                <span className="relative w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent">
+                  {getIcon(icon)}
                 </span>
+              </NavLink>
+              
+              <span
+                className={clsx(
+                  "px-2 py-1 bg-background/30 backdrop-blur-sm text-foreground text-sm rounded-md shadow-lg whitespace-nowrap",
+                  labelDirection === "left" ? "order-first" : "order-last"
+                )}
+              >
+                {label}
               </span>
-            </NavLink>
+            </div>
           </div>
         );
       }}
